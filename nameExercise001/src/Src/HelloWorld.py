d05e76f016f96ca5dfb3455d1896d4c776e095eb
Roman Ramirez <lordpdc@hotmail.com>
2017-10-28 12:15:02 -0500

    First exercise Advance

---
+++ nameExercise001/src/Src/HelloWorld.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+#encoding: windows-1252
+
+def helloWorld():
+    #Write your code here
+    #please print "Hello World!"
+    print "Hello World!"
+
+if __name__ == "__main__":
+    helloWorld()
---
+++ nameExercise001/src/Test/HelloWorldTest.py
@@ -0,0 +1,26 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+import unittest
+import sys
+import StringIO
+from tmc import points
+from Src.HelloWorld import helloWorld
+
+@points('1.0')
+class  HelloWorldTestCase(unittest.TestCase):
+    __qualname__="HelloWorldTest"
+
+    def test_helloWorld(self):
+        awaitedValue="Hello World!\n"
+        capturedOutput=StringIO.StringIO()
+        sys.stdout=capturedOutput
+        helloWorld()
+        sys.stdout=sys.__stdout__
+        self.assertEqual(capturedOutput.getvalue(),awaitedValue,msg="you need print \"Hello World!\"")
+        
+
+if __name__ == '__main__':
+    unittest.main()
+
